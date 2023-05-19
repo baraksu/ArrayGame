7207a8c8e6af3d06b53ac2f443261f8db4a5f2f1
yehogdev <92671186+yehogdev@users.noreply.github.com>
2023-05-19 13:20:03 +0300

    Add files via upload
    Added the prime function.

--- ArrayGame.asm
+++ ArrayGame.asm
@@ -3,6 +3,7 @@
 
 .DATA
     
+	
 	; K has the number of elements in the original array (Here it is 15).
     k db 15d
 	
@@ -19,6 +20,11 @@
     arrMsg db 'Here is Array2: ', '$'
     eleMsg db ', ', '$'
 	endMsg db 13,10,'The missing number is: ', '$'
+	 
+	; Those are the messages I print to the screen.	
+    primeMsg db 13,10,'And it is a prime number!', '$'
+	compMsg db 13,10,'And it is a composite number!', '$'  
+
 
 .CODE
 
@@ -161,6 +167,11 @@ Start:
 	; Prints the missing number. 
     push dx
     call PrintNumber
+	
+	
+	; Check if it is prime.
+	push dx
+    call IsPrime
      
      
           
@@ -304,6 +315,72 @@ proc PrintDigit
 endp PrintDigit
       
       
+	  
       
-	
+; IsPrime:
+; Input: A decimal number.
+; Proccess: The procedure loops throguh every integer that is smaller then the number and check if the number is divisble by it. If it is - it is not prime.     
+; Output: "And it is a prime number!" if it is prime and "And it is a composite number!e" if it is not.  
+
+proc IsPrime
+    
+    push dx
+    push cx 
+    push ax 
+    
+    push bp
+    mov bp, sp   
+       
+       
+       
+    mov cx, [bp+10]         
+          
+    primeCheck:
+        
+       cmp cx, 2
+       je prime
+      
+       mov ax, [bp+10]
+       dec cx
+       div cl
+       cmp ah, 0
+       je composite
+       inc cx
+       loop primeCheck
+           
+       
+           
+           
+    prime:
+    	mov ah, 09h
+    	lea dx, primeMsg
+    	int 21h
+        jmp endPrime 
+        
+       
+    
+    
+    composite:
+    	mov ah, 09h
+    	lea dx, compMsg
+    	int 21h 
+           
+    
+    
+    endPrime:
+        pop bp
+        pop ax
+        pop cx
+        pop dx 
+             
+             
+             
+    
+    ret 2      
+    
+endp IsPrime	
+
+
+
+
 END Start
